1eadf8d8a62be1ec324f06c1aeec8388993a0ccd
Thomas Hahn <thomas.hahn01@gmail.com>
2023-09-21 12:23:11 -0400

    Enable OSX ARM builds for cross compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,12 @@
 mkdir build
 cd build
 
+# Specific setup for cross-compilation
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" ]]; then
+  # Openmpi
+  export OPAL_PREFIX="$PREFIX"
+fi
+
 # Openmpi Specific environment setup - Cf. https://github.com/conda-forge/libnetcdf-feedstock/pull/80
 export OMPI_MCA_btl=self,tcp
 export OMPI_MCA_plm=isolated
@@ -13,11 +19,17 @@ mpiexec="mpiexec --allow-run-as-root"
 export CXXFLAGS="$CXXFLAGS -D_LIBCPP_DISABLE_AVAILABILITY"
 source $PREFIX/share/triqs/triqsvars.sh
 
-cmake \
+cmake ${CMAKE_ARGS} \
+    -DCMAKE_CXX_COMPILER=${BUILD_PREFIX}/bin/$(basename ${CXX}) \
+    -DCMAKE_C_COMPILER=${BUILD_PREFIX}/bin/$(basename ${CC}) \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_BUILD_TYPE=Release \
     ..
 
 make -j${CPU_COUNT} VERBOSE=1
-CTEST_OUTPUT_ON_FAILURE=1 ctest
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
+  CTEST_OUTPUT_ON_FAILURE=1 ctest
+fi
+
 make install
